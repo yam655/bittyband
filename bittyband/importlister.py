6980eb09d1249b8bbd5c498a93ee4549411c296e
Steven Black <yam655@gmail.com>
2017-02-20 22:18:29 -0500

    Trying to sort this out...

--- bittyband/importlister.py
+++ bittyband/importlister.py
@@ -199,7 +199,7 @@ class ImportLister:
             return
         self.data = {}
         sortable_order = []
-        for f in import_dir.glob("*.[mMfF][pPlL]*[3cC]"):
+        for f in import_dir.glob("*.[mM][pP]3"):
             metadata_file = f.with_suffix(".meta")
             lyrics_file = f.with_suffix(".txt")
             media_file = f.resolve()
